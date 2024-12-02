c89667e039064e0789e145c76a9aba5ddaa60336
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-12-02 18:58:43 +0500

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+
+export default function ImageGallery(){
+  return (
+    <SafeAreaView style={{flex:1}}>
+        <Text>Image Gallery</Text>
+    </SafeAreaView>
+  )
+}
