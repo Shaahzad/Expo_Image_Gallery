d8922d0442835bf8b279591b1961dec177d83cf2
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-12-04 23:15:12 +0500

    Done

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Image, FlatList, View, Alert} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Entypo from '@expo/vector-icons/Entypo';
 import * as ImagePicker from 'expo-image-picker';
 import axios from "axios";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const imagesArr = [
     'https://images.unsplash.com/photo-1614020661596-366a1afbb319?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW9iaWxlJTIwQXBwfGVufDB8fDB8fHww',
     'https://images.unsplash.com/photo-1663153204614-6dfc8feebbf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bW9iaWxlJTIwQXBwfGVufDB8fDB8fHww',
@@ -13,9 +13,17 @@ const imagesArr = [
     'https://plus.unsplash.com/premium_photo-1722209813944-a4ee13b7bfd8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fG1vYmlsZSUyMEFwcHxlbnwwfHwwfHx8MA%3D%3D'
 ]
 export default function ImageGallery(){
-    const [image,setImage] = useState(imagesArr)
-
-    const cloudinaryUrl = `https://api.cloudinary.com/v1_1/dqeneupli/image/upload`;
+    const [image,setImage] = useState(imagesArr);
+    useEffect(()=>{
+    const loadImage = async () =>{
+      const savedImage = await AsyncStorage.getItem('images');
+      if (savedImage) {
+        setImage((prevImages) => [...prevImages, savedImage]);
+      }
+    }
+    loadImage()
+    },[])
+    const cloudinaryUrl = `https://api.cloudinary.com/v1_1/${process.env.YOUR_CLOUD_NAME}/image/upload`;
     const uploadImageToCloudinary = async (imageuri: any): Promise<void> => {
         if (!image) {
           Alert.alert('No image selected', 'Please select an image first');
@@ -37,7 +45,10 @@ export default function ImageGallery(){
               'Content-Type': 'multipart/form-data',
             },
           });
-          setImage(response.data.secure_url)
+          const imageUrl = response.data.secure_url;
+          setImage((prevImages) => [...prevImages, imageUrl]);
+          await AsyncStorage.setItem('images', imageUrl);
+          console.log('Image URL:', imageUrl);
           Alert.alert('Success', 'Image uploaded successfully');
           console.log('Cloudinary Response:', response.data);
         } catch (error) {
@@ -95,7 +106,7 @@ export default function ImageGallery(){
         renderItem={({item})=>{
             return(
                 <Image style={{height: 230, marginVertical: 10}}
-                source={{uri: item}}
+                source={{uri:item}}
                 />
             )
         }}
